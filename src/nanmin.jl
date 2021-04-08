214de0625b58c0e610b6c71fb085c56c7f27a05d
Jeremy <jeremy.rueffer@thuenen.de>
2021-04-08 10:27:17 +0200

    Update nanmin.jl
    Update for changes in mapslices

--- src/nanmin.jl
+++ src/nanmin.jl
@@ -6,12 +6,12 @@
 # Thünen Institut
 # Institut für Agrarklimaschutz
 # Junior Research Group NITROSPHERE
-# Julia 1.4.0
+# Julia 1.6.0
 # 02.04.2020
-# Last Edit: 02.04.2020
+# Last Edit: 08.04.2021
 
 nanmin(x::Array{T}) where T <: Number = minimum(filter(!isnan,x))
 
 nanmin(x::Array{Union{T,Missing}}) where T <: Number = minimum(filter(!isnan,filter(!ismissing,x)))
 
-nanmin(x::Array{T},y::Integer) where T <: Number = mapslices(nanmin,x,y)
+nanmin(x::Array{T},y::Integer) where T <: Number = mapslices(nanmin,x,dims=y)
