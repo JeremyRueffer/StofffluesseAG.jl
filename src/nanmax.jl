e6c99b7d79d476cf76254e96b9f92230f4327a14
Jeremy <jeremy.rueffer@thuenen.de>
2021-04-08 10:26:14 +0200

    Update nanmax.jl
    Update for changes in mapslices

--- src/nanmax.jl
+++ src/nanmax.jl
@@ -6,12 +6,12 @@
 # Thünen Institut
 # Institut für Agrarklimaschutz
 # Junior Research Group NITROSPHERE
-# Julia 1.4.0
+# Julia 1.6.0
 # 02.04.2020
-# Last Edit: 02.04.2020
+# Last Edit: 08.04.2021
 
 nanmax(x::Array{T}) where T <: Number = maximum(filter(!isnan,x))
 
 nanmax(x::Array{Union{T,Missing}}) where T <: Number = maximum(filter(!isnan,filter(!ismissing,x)))
 
-nanmax(x::Array{T},y::Integer) where T <: Number = mapslices(nanmax,x,y)
+nanmax(x::Array{T},y::Integer) where T <: Number = mapslices(nanmax,x,dims=y)
