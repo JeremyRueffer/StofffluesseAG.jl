2920e7f9c75ca85bc04f636150b233baa643df83
Jeremy <jeremy.rueffer@thuenen.de>
2021-04-08 10:26:46 +0200

    Update nanmean.jl
    Update for changes in mapslices

--- src/nanmean.jl
+++ src/nanmean.jl
@@ -6,9 +6,9 @@
 # Thünen Institut
 # Institut für Agrarklimaschutz
 # Junior Research Group NITROSPHERE
-# Julia 1.4.0
+# Julia 1.6.0
 # 02.04.2020
-# Last Edit: 02.04.2020
+# Last Edit: 08.04.2021
 
 nanmean(x::Array{T}) where T <: Number = mean(filter(!isnan,x))
 
@@ -16,4 +16,4 @@ nanmean(x::Array{Union{T,Missing}}) where T <: Number = mean(filter(!isnan,filte
 
 nanmean(x::Array{T},y::Integer) where T <: Number = mean(filter(!isnan,x,y))
 
-nanmean(x::Array{Union{T,Missing}},y::Integer) where T <: Number = mapslices(nanmean,x,y)
+nanmean(x::Array{Union{T,Missing}},y::Integer) where T <: Number = mapslices(nanmean,x,dims=y)
